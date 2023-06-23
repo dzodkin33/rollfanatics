63ff71e6b07ff9cb3f2ff3ed54bccaf2450a5470
Boris Ryabov <stonks33@gmail.com>
2023-06-23 21:38:36 +0100

    Initial Commit

---
+++ rollfanatics/rollfanaticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  rollfanaticsApp.swift
+//  rollfanatics
+//
+//  Created by Борис Рябов on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct rollfanaticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
