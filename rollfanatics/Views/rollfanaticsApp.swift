2fe62793cae1d1d8846bffd3c4915668710f8a14
Boris Ryabov <stonks33@gmail.com>
2023-07-15 20:48:23 +0100

    [ADD] new app initialization

--- rollfanatics/Views/rollfanaticsApp.swift
+++ rollfanatics/Views/rollfanaticsApp.swift
@@ -10,7 +10,30 @@ import SwiftUI
 @main
 struct rollfanaticsApp: App {
     @State var positions: [PositionRecord] = PositionRecord.sampleRecord
-    @State var records:[TechniqueRecord] = TechniqueRecord.sampleData// todo: populate here 
+    @State var records:[TechniqueRecord] = TechniqueRecord.sampleData// todo: populate here
+    @State var recordBindings:[PositionTechniqueBinding] =  []
+    
+    // TODO: here we also have to load up data from
+    init() {
+        
+        
+        // Performs binding population
+        for record in records {
+            let pos: PositionRecord = record.position
+            var binding = recordBindings.first(where: {$0.position == pos})
+            
+            if (binding != nil) {
+                binding?.addAssosiatedTechnique(recordId: record.id)
+            } else {
+                let binding = PositionTechniqueBinding(
+                position: pos,
+                listOfTechniques: [record.id])
+                recordBindings.append(binding)
+            }
+        }
+        
+    }
+    
     var body: some Scene {
         WindowGroup {
             TechniqueListView(records: $records, positions: $positions)
